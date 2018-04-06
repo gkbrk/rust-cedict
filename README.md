f036c355c1d7e05e8680635160db7732110b3aab
Gökberk Yaltıraklı <webdosusb@gmail.com>
2018-04-06 20:20:51 +0100

    documentation: Improved documentation
    * Added examples to DictEntry methods
    * Updated README with another example
    * Added more doctests

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # cedict - Rust library for parsing CC-CEDICT
 
-[![Crates.io](https://img.shields.io/crates/v/cedict.svg)](https://crates.io/crates/cedict) [![Docs.rs](https://img.shields.io/badge/docs-cedict-brightgreen.svg)](https://docs.rs/cedict)
+[![Crates.io](https://img.shields.io/crates/v/cedict.svg)](https://crates.io/crates/cedict)
+[![Docs.rs](https://img.shields.io/badge/docs-cedict-brightgreen.svg)](https://docs.rs/cedict)
 
+cedict is a Rust crate for reading and writing the CC-CEDICT Chinese-English
+dictionary format. It can be used to implement Chinese dictionaries in Rust. It
+can also serve as a tool for automating maintenance to the CEDICT project.
 
 ## What is CC-CEDICT
-CC-CEDICT, or formerly CEDICT, is a freely available Chinese-English dictionary. This library allows you to parse it.
+CC-CEDICT, or formerly CEDICT, is a freely available Chinese-English
+dictionary. This library allows you to parse it.
 
 ## Usage
 
@@ -13,4 +18,22 @@ let line = "你好 你好 [ni3 hao3] /Hello!/Hi!/How are you?/";
 let parsed = cedict::parse_line(line).unwrap();
 
 println!("{}", parsed.definitions[0]); // Prints "Hello!"
+```
+
+Parse a dictionary file and search for "Hello".
+
+```rust
+extern crate cedict;
+
+use std::fs::File;
+
+fn main() {
+    let file = File::open("cedict.txt").unwrap();
+
+    for definition in cedict::parse_reader(file) {
+        if definition.definitions().next().unwrap().contains("Hello") {
+            println!("{}", definition.simplified());
+        }
+    }
+}
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -29,7 +29,7 @@
 //!
 //! ```
 
-use std::io::{BufReader, BufRead, Read};
+use std::io::{BufRead, BufReader, Read};
 
 /// A struct that contains all fields of a CEDICT definition
 #[derive(Debug)]
@@ -38,26 +38,58 @@ pub struct DictEntry {
     traditional: Slice,
     simplified: Slice,
     pinyin: Slice,
-    definitions: Vec<Slice>
+    definitions: Vec<Slice>,
 }
 
 type Slice = (usize, usize);
 
 impl DictEntry {
     /// Gets the traditional Chinese characters for the entry.
+    ///
+    /// ```
+    /// let line = "學習 学习 [xue2 xi2] /to learn/to study/";
+    /// let dict = cedict::parse_line(line).unwrap();
+    ///
+    /// assert_eq!(dict.traditional(), "學習");
+    /// ```
     pub fn traditional(&self) -> &str {
         self.slice(&self.traditional)
     }
 
+    /// Gets the simplified traditional characters for the entry.
+    ///
+    /// ```
+    /// let line = "學習 学习 [xue2 xi2] /to learn/to study/";
+    /// let dict = cedict::parse_line(line).unwrap();
+    ///
+    /// assert_eq!(dict.simplified(), "学习");
+    /// ```
     pub fn simplified(&self) -> &str {
         self.slice(&self.simplified)
     }
 
+    /// Gets the pinyin form of the entry.
+    ///
+    /// ```
+    /// let line = "學習 学习 [xue2 xi2] /to learn/to study/";
+    /// let dict = cedict::parse_line(line).unwrap();
+    ///
+    /// assert_eq!(dict.pinyin(), "xue2 xi2");
+    /// ```
     pub fn pinyin(&self) -> &str {
         self.slice(&self.pinyin)
     }
 
-    pub fn definitions(&self) -> impl Iterator<Item=&str> {
+    /// Returns an iterator over the definitions of the entry.
+    ///
+    /// ```
+    /// let line = "學習 学习 [xue2 xi2] /to learn/to study/";
+    /// let dict = cedict::parse_line(line).unwrap();
+    ///
+    /// assert_eq!(dict.definitions().nth(0), Some("to learn"));
+    /// assert_eq!(dict.definitions().nth(1), Some("to study"));
+    /// ```
+    pub fn definitions(&self) -> impl Iterator<Item = &str> {
         self.definitions.iter().map(move |x| self.slice(x))
     }
 
@@ -65,6 +97,8 @@ impl DictEntry {
         &self.line[slice.0..slice.1]
     }
 
+    /// Creates a new `DictEntry`. This can be used to add new entries to the
+    /// file.
     pub fn new(trad: &str, simp: &str, pinyin: &str, defs: Vec<&str>) -> DictEntry {
         let mut line = String::new();
 
@@ -82,14 +116,13 @@ impl DictEntry {
 
         parse_line(line).unwrap()
     }
-    
+
     /// Formats a DictEntry into a CEDICT formatted line. This function can be
     /// used to modify or create CEDICT files.
     pub fn to_string(&self) -> &str {
         &self.line
     }
 }
-
 
 /// Parses a line in the CEDICT format into a `DictEntry`
 ///
@@ -107,7 +140,7 @@ pub fn parse_line<S: Into<String>>(line: S) -> Result<DictEntry, ()> {
     let line_orig = line;
     let lineptr = line.as_ptr();
     let linelen = line.len();
-    
+
     // Handle file comments
     // They are currently ignored
     if line.starts_with('#') {
@@ -116,18 +149,18 @@ pub fn parse_line<S: Into<String>>(line: S) -> Result<DictEntry, ()> {
 
     let (traditional, line) = {
         let mut parts = line.splitn(2, ' ');
-        ( parts.next().ok_or(())?, parts.next().ok_or(())? )
+        (parts.next().ok_or(())?, parts.next().ok_or(())?)
     };
 
     let (simplified, line) = {
         let mut parts = line.splitn(2, ' ');
-        ( parts.next().ok_or(())?, parts.next().ok_or(())? )
+        (parts.next().ok_or(())?, parts.next().ok_or(())?)
     };
 
     let (pinyin, line) = {
         let pinyin_begin = line.find('[').ok_or(())? + 1;
         let pinyin_end = line.find(']').ok_or(())?;
-        ( &line[pinyin_begin..pinyin_end], &line[pinyin_end+1..] )
+        (&line[pinyin_begin..pinyin_end], &line[pinyin_end + 1..])
     };
 
     let toslice = |a: &str| {
@@ -148,13 +181,13 @@ pub fn parse_line<S: Into<String>>(line: S) -> Result<DictEntry, ()> {
         }
         defs
     };
-    
+
     Ok(DictEntry {
         line: line_orig.to_string(),
         traditional: toslice(traditional),
         simplified: toslice(simplified),
         pinyin: toslice(pinyin),
-        definitions
+        definitions,
     })
 }
 
@@ -169,15 +202,17 @@ pub fn parse_line<S: Into<String>>(line: S) -> Result<DictEntry, ()> {
 ///     Ok(f) => f,
 ///     Err(_) => { return; }
 /// };
-/// 
+///
 /// for dict_entry in cedict::parse_reader(f) {
 ///     println!("Read the definition of {}. It means {}.", dict_entry.simplified(),
 ///       dict_entry.definitions().next().unwrap());
 /// }
 /// ```
-pub fn parse_reader<T: Read>(f: T) -> impl Iterator<Item=DictEntry> {
+pub fn parse_reader<T: Read>(f: T) -> impl Iterator<Item = DictEntry> {
     let bufread = BufReader::new(f);
-    bufread.lines().filter_map(|x| x.ok())
+    bufread
+        .lines()
+        .filter_map(|x| x.ok())
         .map(parse_line)
         .filter_map(|x| x.ok())
 }
@@ -219,13 +254,13 @@ fn test_parse_reader() {
                 assert_eq!(word.traditional(), "你好");
                 assert_eq!(word.pinyin(), "ni3 hao3");
                 assert_eq!(word.definitions().next(), Some("Hello!"));
-            },
+            }
             1 => {
                 assert_eq!(word.simplified(), "爱");
                 assert_eq!(word.traditional(), "愛");
                 assert_eq!(word.pinyin(), "ai4");
                 assert_eq!(word.definitions().nth(1), Some("to be fond of"));
-            },
+            }
             _ => {}
         }
     }
